9348283bf5b3ff7593538502d227c760daf66662
ttacon <ttacon@gmail.com>
2014-07-19 08:54:09 -0400

    Clean up the api

--- colorize.go
+++ colorize.go
@@ -2,63 +2,80 @@ package colorize
 
 import "fmt"
 
-type Color interface {
-	Foreground(*int) int
-	Background(*int) int
-	Color(string) string
-	WithBackground(Color) Color
+type Color struct {
+	value int
 }
 
-// TODO(ttacon): add styles at some point (when we care enough about them)
-
-type color struct {
-	value      int
-	background *int
+type Style interface {
+	Foreground(Color)
+	Background(Color)
+	Style(string) string
+	WithBackground(Color) Style
+	WithForeground(Color) Style
 }
 
-func (c *color) WithBackground(col Color) Color {
-	bg := col.Foreground(nil)
-	c.Background(&bg)
-	return c
+type style struct {
+	foreground Color
+	background Color
+	// TODO(ttacon): add styles at some point (when we care enough about them)
 }
 
-func (c *color) String() string {
-	var toReturn string
-	if c.background != nil {
-		toReturn = fmt.Sprintf("\u001b[%dm", 40+*c.background)
-	}
-	return toReturn + fmt.Sprintf("\u001b[%dm", 30+c.value)
-}
-
-func (c *color) Color(val string) string {
-	return fmt.Sprintf("%s%s%s", c, val, Reset)
-}
-
-func (c *color) Foreground(val *int) int {
-	if val != nil {
-		c.value = *val
-	}
+func (c Color) Value() int {
 	return c.value
 }
 
-func (c *color) Background(val *int) int {
-	if val != nil {
-		temp := *val
-		c.background = &temp
-	}
-	return *c.background
+func (c Color) Color(val string) string {
+	return fmt.Sprintf("%s%s%s", c, val, ResetColor)
+}
+
+func (c Color) String() string {
+	return fmt.Sprintf("\u001b[%dm", 30+c.value)
+}
+
+func (c Color) NewStyle() Style {
+	return &style{foreground: c}
+}
+
+func (s *style) WithBackground(col Color) Style {
+	s.Background(col)
+	return s
+}
+
+func (s *style) WithForeground(col Color) Style {
+	s.Foreground(col)
+	return s
+}
+
+func (s *style) String() string {
+	var toReturn string
+	toReturn = fmt.Sprintf("\u001b[%dm", 40+s.background.Value())
+	return toReturn + fmt.Sprintf("\u001b[%dm", 30+s.foreground.Value())
+}
+
+func (s *style) Style(val string) string {
+	return fmt.Sprintf("%s%s%s", s, val, Reset)
+}
+
+func (s *style) Foreground(col Color) {
+	s.foreground = col
+}
+
+func (s *style) Background(col Color) {
+	s.background = col
 }
 
 var (
 	nine = 9
 
-	Black   = &color{0, nil}
-	Red     = &color{1, nil}
-	Green   = &color{2, nil}
-	Yellow  = &color{3, nil}
-	Blue    = &color{4, nil}
-	Magenta = &color{5, nil}
-	Cyan    = &color{6, nil}
-	White   = &color{7, nil}
-	Reset   = &color{9, &nine}
+	Black      = Color{0}
+	Red        = Color{1}
+	Green      = Color{2}
+	Yellow     = Color{3}
+	Blue       = Color{4}
+	Magenta    = Color{5}
+	Cyan       = Color{6}
+	White      = Color{7}
+	ResetColor = Color{9}
+
+	Reset = &style{ResetColor, ResetColor}
 )
--- examples/main.go
+++ examples/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 
-	"github.com/ttacon/goji-colorize/colorize"
+	"github.com/ttacon/colorize"
 )
 
 func main() {
+	// You can just use colors
 	fmt.Println(colorize.Red, "Writing in colors", colorize.Cyan, "is so much fun", colorize.Reset)
 	fmt.Println(colorize.Magenta.Color("You can use colors to color specific phrases"))
-	fmt.Printf("%s%s%s\n", colorize.Blue.WithBackground(colorize.White), "And they also have backgrounds!", colorize.Reset)
+
+	// Or you can use styles
+	blueOnWhite := colorize.Blue.NewStyle().WithBackground(colorize.White)
+	fmt.Printf("%s%s%s\n", blueOnWhite, "And they also have backgrounds!", colorize.Reset)
+	fmt.Println(blueOnWhite.Style("You can style strings the same way you can color them!"))
 }
