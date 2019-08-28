4447be252e472d21a0bc00800a31103d9499fe90
Marcus Kazmierczak <marcus@mkaz.com>
2019-08-28 16:55:20 -0700

    Move vars to shared file (platform agnostic)

--- chalk_windows.go
+++ chalk_windows.go
@@ -124,34 +124,3 @@ func (s *style) WithTextStyle(textStyle TextStyle) Style {
 	s.textStyle = textStyle
 	return s
 }
-
-var (
-	// Colors
-
-	Black      = Color{0}
-	Red        = Color{1}
-	Green      = Color{2}
-	Yellow     = Color{3}
-	Blue       = Color{4}
-	Magenta    = Color{5}
-	Cyan       = Color{6}
-	White      = Color{7}
-	ResetColor = Color{9}
-
-	// Text Styles
-
-	Bold          = TextStyle{1, 22}
-	Dim           = TextStyle{2, 22}
-	Italic        = TextStyle{3, 23}
-	Underline     = TextStyle{4, 24}
-	Inverse       = TextStyle{7, 27}
-	Hidden        = TextStyle{8, 28}
-	Strikethrough = TextStyle{9, 29}
-
-	Reset = &style{
-		foreground: ResetColor,
-		background: ResetColor,
-	}
-
-	emptyTextStyle = TextStyle{}
-)
